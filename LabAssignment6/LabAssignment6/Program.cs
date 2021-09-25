66417eff89e2e34f33eb1854015473e2c6c1a998
Rachana <giliyalrachana@gmail.com>
2021-09-25 19:33:40 +0530

    New classes are added

---
+++ LabAssignment6/LabAssignment6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace LabAssignment6
+{
+    class CityInfo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("City Names\n");
+            var city = Enum.GetNames(typeof(CityInfo));
+            foreach (var name in city)
+            {
+                Console.WriteLine($"{name}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Zip Codes");
+            var cityname = Enum.GetValues(typeof(CityInfo));
+            foreach (var name in cityname)
+            {
+                Console.WriteLine($"{name.GetHashCode}");
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
